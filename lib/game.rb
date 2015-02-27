b1f01a4531372d24b37175a4123a74ae0e6413a5
Miriam Tocino <miriam.tocino@gmail.com>
2015-02-27 16:04:08 +0100

    Using polymorphism so that game communicates with hand and dealer in the same way (though their behaviors are different).Using encapsulation so that game data is now private.

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,6 @@ require_relative 'hand'
 require_relative 'dealer'
 
 class Game
-  attr_accessor :hand, :dealer
 
   def initialize
     puts "WELCOME TO BLACKJACK!"
@@ -11,29 +10,57 @@ class Game
     @deck = Deck.new
     @hand = Hand.new
     @dealer = Dealer.new
+    @is_finished = false
 
     2.times do
-      give_card_to_hand
+      give_card_to(@hand)
     end
-
     puts "*" * 40
-
     2.times do
-      give_card_to_dealer
+      give_card_to(@dealer)
     end
   end
 
-  def do_you_want_a_card(player)
-    player.decide
+  def update
+    give_turn_to(@hand)
+    give_turn_to(@dealer)
+    resolution
   end
 
-  def give_card_to_hand
-    card = @deck.draw
-    @hand.receive_card(card)
+private
+
+  def give_turn_to(player)
+    puts "*" * 40 + "\n#{player.class.name.upcase} TURN" unless @is_finished
+    while !@is_finished && player.decide
+      give_card_to(player)
+
+      if player.score > 21
+        if player == @hand
+          puts "*" * 40 + "\nYou lost, your current score is higher than 21: #{player.score.to_s}"
+        elsif player == @dealer
+          puts "*" * 40 + "\nYou won, the dealer's score is higher than 21: #{player.score.to_s}"
+        end
+        @is_finished = true
+      end
+    end
   end
 
-  def give_card_to_dealer
+  def give_card_to(player)
     card = @deck.draw
-    @dealer.receive_card(card)
+    player.receive_card(card)
+  end
+
+  def resolution
+    unless @is_finished == true
+      puts "*" * 40 + "\nGAME'S RESOLUTION"
+      puts "Hand Score: #{@hand.score.to_s}"
+      puts "Dealer Score: #{@dealer.score.to_s}"
+      if @dealer.score > @hand.score
+        puts "You lost, the dealer's score is higher than yours."
+      else
+        puts "You won, the dealer's score is lower than yours."
+      end
+    end
+
   end
 end
